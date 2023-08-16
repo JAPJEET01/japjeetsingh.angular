2fe9c7fedfb38daccbab009401f6e78b0c6db9e2
JAPJEET SINGH <japjeet612003@gmail.com>
2023-08-16 19:02:03 +0530

    update

--- japjeetsingh-angular/src/app/app.module.ts
+++ japjeetsingh-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +31,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+    positionClass: 'toast-top-right',
+    preventDuplicates: true,
+    }), // ToastrModule added
 
   ],
   providers: [],
--- japjeetsingh-angular/src/app/login/login.component.ts
+++ japjeetsingh-angular/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-login',
@@ -12,11 +14,27 @@ export class LoginComponent implements OnInit{
     email: new FormControl("",[Validators.required,Validators.email]),
     password:  new FormControl("",[Validators.required,Validators.minLength(5)])
   })
-  constructor(private router : Router){}
+  constructor(private router : Router ,private toastr: ToastrService){}
   ngOnInit(): void {
   }
 
   submit(){
+    console.log(this.loginform.value)
+    if(this.loginform.value.email =="japjeet@gmail.com" && this.loginform.value.password=="123456" ){
+      setTimeout(() => {
+        this.toastr.success('Hello world!', 'Toastr fun!');
+      }, 4000);
+      
+    }
+    else{
+      setTimeout(() => {
+        this.toastr.error('Hello world!', 'Toastr fun!');
+      }, 4000);
+      
+    }
     
+  }
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
   }
 }
